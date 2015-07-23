61958d467bdf81729060e3660a11e94e386cafce
matheus <contato@matheusgimenez.com.br>
2015-07-23 14:57:37 -0300

    issue #9

--- content-page.php
+++ content-page.php
@@ -6,9 +6,20 @@
  * @since 2.2.0
  */
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php $class = ''; ?>
+<?php if( !has_post_thumbnail() ) $class = 'no-thumb';?>
+<?php if( !is_page() ): ?>
+	<div class="col-md-12 data-caption <?php echo $class;?>">
+		<div class="the-data">
+			<?php echo sprintf( __('%s de %s de %s'), get_the_date('d'), get_the_date('F'), get_the_date('Y'));?>
+			|
+			<?php the_category('|');?>
+		</div><!-- .col-md-4 the-data -->
+	</div><!-- .col-md-12 data-caption -->
+<?php endif;?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( $class ); ?>>
 	<?php the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' ); ?>
+
 
 	<div class="entry-content">
 		<?php
--- single.php
+++ single.php
@@ -1,25 +1,32 @@
 <?php
 /**
- * The Template for displaying all single posts.
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
  *
  * @package Odin
  * @since 2.2.0
  */
 
 get_header('page'); ?>
-
-	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
-		<main id="main" class="interno site-main" role="main">
+<?php $page = get_queried_object();?>
+<?php if( has_post_thumbnail($page->ID) ): ?>
+	<?php $class = '';?>
+	<?php the_post_thumbnail( 'large', array('class' => 'col-md-12 page-thumbnail') );?>
+<?php else: ?>
+	<?php $class = 'no-thumb';?>
+<?php endif;?>
+<div class="col-md-12 bg-page">
+	<div class="container <?php echo $class;?>">
+		<div class="row">
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
@@ -27,8 +34,60 @@ get_header('page'); ?>
 					endif;
 				endwhile;
 			?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div><!-- .row -->
+	</div><!-- .container -->
+</div>
+<div class="container">
+		<div class="row" id="destaques">
+			<div id="servicos-interno" class="col-md-12">
+				<?php $style = '';?>
+				<?php if( kirki_get_option('box1_color') ) $style = 'background:' . kirki_get_option('box1_color') . ';';?>
+				<div class="text-center destaque-home polos" style="<?php echo $style;?>">
+					<h3><?php echo kirki_get_option('box1_title');?></h3>
+					<?php if( $text = kirki_get_option('box1_text') ):?>
+						<div class="col-md-12">
+								<p><?php echo $text;?></p>
+						</div><!-- .col-md-12 -->
+				    <?php endif;?>
+					<?php if( kirki_get_option('box1_btn_url') ):?>
+						<a class="btn btn-primary" href="<?php echo esc_url( kirki_get_option('box1_btn_url') );?>">
+							<?php echo kirki_get_option('box1_btn');?>
+						</a>
+					<?php endif;?>
+				</div>
+				<?php $style = '';?>
+				<?php if( kirki_get_option('box2_color') ) $style = 'background:' . kirki_get_option('box2_color') . ';';?>
+				<div class="text-center destaque-home rede-paulista" style="<?php echo $style;?>">
+					<h3><?php echo kirki_get_option('box2_title');?></h3>
+					<?php if( $text = kirki_get_option('box2_text') ):?>
+						<div class="col-md-12">
+							<p><?php echo $text;?></p>
+						</div><!-- .col-md-12 -->
+					<?php endif;?>
+					<?php if( kirki_get_option('box2_btn_url') ):?>
+						<a class="btn btn-primary" href="<?php echo esc_url( kirki_get_option('box2_btn_url') );?>">
+							<?php echo kirki_get_option('box2_btn');?>
+						</a>
+					<?php endif;?>
+				</div>
+				<div class="text-center destaque-home copa-america">
+					<?php if( $title = kirki_get_option('participe_title') ): ?>
+						<h3><?php echo apply_filters('the_title', $title);?></h3>
+					<?php endif;?>
+					<?php if( $text = kirki_get_option('participe_text') ):?>
+						<div class="col-md-12">
+							<p><?php echo apply_filters('the_content', $text);?></p>
+						</div><!-- .col-md-12 -->
+					<?php endif;?>
+					<?php if( $url = kirki_get_option('participe_btn_url') ):?>
+						<a class="btn btn-primary" href="<?php echo esc_url( $url );?>">
+							<?php echo kirki_get_option('participe_btn');?>
+						</a>
+					<?php endif;?>
+				</div>
+			</div><!--servicos interno-->
+		</div><!--destaques-->
+	</div><!-- .container -->
 
 <?php
 get_footer('page');
